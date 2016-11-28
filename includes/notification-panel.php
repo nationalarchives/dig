16984bd71ea672c43ea61e1d3d3cbc26c3242d15
Gwyn Jones <gwyn.jones@nationalarchives.gsi.gov.uk>
2016-11-28 12:19:47 +0000

    Introduce basic structure
    Adds basic structure (using example provided by Bootstrap) with key
    areas separated into individual PHP includes. Index page loads:
    
    * jQuery
    * Bootstrap
    * Dashboard styles (remotely)

---
+++ includes/notification-panel.php
@@ -0,0 +1,6 @@
+<div id="navbar" class="navbar-collapse collapse">
+    <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">{{ item }}</a></li>
+        <li><a href="#">{{ item }}</a></li>
+    </ul>
+</div>
---
+++ index.php
@@ -0,0 +1,42 @@
+<html lang="en">
+<head>
+
+    <?php require_once('includes/meta.php'); ?>
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>{{ title }}</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+
+    <!-- Custom styles for this template -->
+    <link href="http://getbootstrap.com/examples/dashboard/dashboard.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+<nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container-fluid">
+        <?php require_once('includes/logo.php') ?>
+        <?php require_once('includes/notification-panel.php') ?>
+    </div>
+</nav>
+
+<div class="container-fluid">
+    <div class="row">
+        <?php require_once('includes/sidebar.php') ?>
+        <?php require_once('includes/main-content.php') ?>
+    </div>
+</div>
+
+<script src="bower_components/jquery/dist/jquery.min.js"></script>
+<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+
+</body>
+</html>
