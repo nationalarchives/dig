0ae7d8045652a05f449aaf5588a117b0c3943022
Punal Chotrani <punalsc@gmail.com>
2016-11-30 17:45:40 +0000

    Removed a tag from panel, Added buttons into panel.

--- includes/transfer.php
+++ includes/transfer.php
@@ -1,28 +1,28 @@
-<a href="#">
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            <?php
-                $letters = range('A','Z');
-                $k = array_rand($letters);
-                $v = $letters[$k];
-            ?>
+<div class="panel panel-default">
+    <div class="panel-heading">
+		<?php
+            $letters = range('A','Z');
+            $k = array_rand($letters);
+            $v = $letters[$k];
+		?>
+        <a href="#">
             <h3 class="panel-title"><?php echo 'Transfer '.$v; ?></h3>
+        </a>
+    </div>
+    <div class="panel-body">
+        <div class="col-md-1">
+            <span class="glyphicon glyphicon-transfer" aria-hidden="true"></span>
         </div>
-        <div class="panel-body">
-            <div class="col-md-1">
-                <?php
-                    $array = array('glyphicon-cloud-upload','glyphicon-hdd');
-                    $k = array_rand($array);
-                    $v = $array[$k];
-                ?>
-                <span class="glyphicon <?php echo $v; ?>" aria-hidden="true"></span>
+        <div class="col-md-11">
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+            <div class="btn-group pull-right" role="group" aria-label="...">
+                <button type="button" class="btn btn-default">Button 1</button>
+                <button type="button" class="btn btn-default">Button 2</button>
+                <button type="button" class="btn btn-default">Button 3</button>
             </div>
-            <div class="col-md-11">
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
-            </div>
-        </div>
-        <div class="panel-footer">
-            Transfer Footer
         </div>
     </div>
-</a>
+    <div class="panel-footer">
+        Transfer Footer
+    </div>
+</div>
