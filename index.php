23bdf16090b8883967bf4dc7061b7891580b0494
Punal Chotrani <punalsc@gmail.com>
2016-12-01 14:17:03 +0000

    Removed magic constant as it's not needed. Updated the template path.

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 require_once 'vendor/autoload.php';
 
-$loader = new Twig_Loader_Filesystem(__DIR__ . '/templates');
+$loader = new Twig_Loader_Filesystem('templates');
 
 $twig = new Twig_Environment($loader, array());
 
