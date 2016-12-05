790be73d3f3ec3e63bc1f71dd9f90ce2eb8a22cd
Punal Chotrani <punalsc@gmail.com>
2016-12-05 16:04:45 +0000

    Added individual transfer details view and route.

--- src/routes.php
+++ src/routes.php
@@ -12,3 +12,12 @@ $app->get('/', function ($request, $response, $args) {
 
     return $this->view->render($response, 'aggregate-transfers.html', $page_info);
 })->setName('profile');
+
+$app->get('/transfer', function ($request, $response, $args) {
+
+	// Instantiate Page object
+	$page = new \tna\Page();
+	$page_info = $page->getPageInfo();
+
+	return $this->view->render($response, 'transfer-details.html', $page_info);
+})->setName('detail');
