ffedb96e4fc90e11f99fe22b20142f14f48c0fbf
Gwyn Jones <m@gwnjns.com>
2016-12-06 17:40:11 +0000

    Simplify routing
    Interim step to simplify routing. This introduces a /metadata-upload
    route which is responsible for presenting the flash messages only.

--- src/routes.php
+++ src/routes.php
@@ -6,7 +6,6 @@ require __DIR__ . '/Page.php';
 
 $app->get('/', function ($request, $response, $args) {
 
-    // Instantiate Page object
     $page = new \tna\Page();
     $page_info = $page->getPageInfo();
 
@@ -15,28 +14,38 @@ $app->get('/', function ($request, $response, $args) {
 
 $app->get('/transfer', function ($request, $response, $args) {
 
-	// Instantiate Page object
-	$page = new \tna\Page();
-	$page_info = $page->getPageInfo();
+    $page = new \tna\Page();
+    $page_info = $page->getPageInfo();
 
-	return $this->view->render($response, 'transfer-details.html', $page_info);
+    return $this->view->render($response, 'transfer-details.html', $page_info);
 })->setName('summary');
 
 $app->post('/transfer', function ($request, $response, $args) {
 
-	// Instantiate Page object
-	$page = new \tna\Page();
-	$page_info = $page->getPageInfo();
-	$page_info['flash_message'] = 'Metadata uploaded successfully!';
-	$upload = $_POST['upload'];
-	$page_info['message_class'] = 'success';
-	if (!empty($upload)) {
-	    $page_info['show_summary'] = true;
-		return $this->view->render($response, 'message.html', $page_info);
-	}
-	if(empty($upload)) {
-		$page_info['flash_message'] = 'Metadata empty';
-		$page_info['message_class'] = 'danger';
-		return $this->view->render($response, 'message.html', $page_info);
-	}
+    // Instantiate Page object
+    $page = new \tna\Page();
+    $page_info = $page->getPageInfo();
+
+    return $this->view->render($response, 'message.html', $page_info);
+
 })->setName('message');
+
+$app->post('/metadata-upload', function ($request, $response, $args) {
+
+    $page = new \tna\Page();
+    $page_info = $page->getPageInfo();
+
+    $upload = $request->getParam('upload');
+
+    if (!empty($upload)) {
+        $page_info['flash_message'] = 'Metadata uploaded successfully!';
+        $page_info['message_class'] = 'success';
+        $page_info['show_summary'] = true;
+    }
+    if (empty($upload)) {
+        $page_info['flash_message'] = 'Metadata empty';
+        $page_info['message_class'] = 'danger';
+    }
+
+    return $this->view->render($response, 'message.html', $page_info);
+})->setName('metadata-upload');
