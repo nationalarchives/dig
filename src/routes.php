c742a63caba110ce5a44179acc56cb4344958530
Gwyn Jones <gwyn.jones@nationalarchives.gsi.gov.uk>
2016-12-20 14:42:11 +0000

    Introduce transfer review page

--- src/routes.php
+++ src/routes.php
@@ -36,25 +36,27 @@ $app->post('/metadata-upload', function ($request, $response, $args) {
             'flash_message_class' => 'success',
             'show_summary' => true
         ]);
+        return $this->view->render($response, 'metadata-review.html.twig', $page->getPageInfo());
     } else {
         $page = new \tna\Page([
             'page_title' => 'Metadata upload failed',
             'flash_message' => 'Metadata empty',
             'flash_message_class' => 'danger'
         ]);
+        return $this->view->render($response, 'message.html.twig', $page->getPageInfo());
     }
 
-    return $this->view->render($response, 'message.html.twig', $page->getPageInfo());
+
 })->setName('metadata-upload');
 
 $app->get('/search-results', function ($request, $response, $args) {
 
-	$page = new \tna\Page(['page_title' => 'Search results']);
+    $page = new \tna\Page(['page_title' => 'Search results']);
 
-	return $this->view->render($response, 'search-results.html.twig', $page->getPageInfo());
+    return $this->view->render($response, 'search-results.html.twig', $page->getPageInfo());
 })->setName('search-results');
 
-$app->get('/begin-transfer', function($request, $response, $args) {
+$app->get('/begin-transfer', function ($request, $response, $args) {
 
     $page = new \tna\Page([
         'page_title' => 'Begin transfer',
@@ -65,36 +67,41 @@ $app->get('/begin-transfer', function($request, $response, $args) {
     return $this->view->render($response, 'transfer-details.html.twig', $page->getPageInfo());
 });
 
-$app->get('/request-hard-drive', function($request, $response, $arg) {
+$app->get('/request-hard-drive', function ($request, $response, $arg) {
 
     $page = new \tna\Page(['page_title' => 'Request Hard Drive']);
 
     return $this->view->render($response, 'request-hard-drive.html.twig', $page->getPageInfo());
 });
 
-$app->get('/record-preview', function($request, $response, $args) {
+$app->get('/record-preview', function ($request, $response, $args) {
 
-	$page = new \tna\Page([
-	    'page_title' => 'Record preview',
-		'show_summary' => true,
-		'begin_transfer' => true
-	]);
+    $page = new \tna\Page([
+        'page_title' => 'Record preview',
+        'show_summary' => true,
+        'begin_transfer' => true
+    ]);
 
-	return $this->view->render($response, 'record-preview.html.twig', $page->getPageInfo());
+    return $this->view->render($response, 'record-preview.html.twig', $page->getPageInfo());
 });
 
 $app->get('/upload', function ($request, $response, $args) {
 
-	// Instantiate Page object
-	$page = new \tna\Page(['page_title' => 'Upload transfer']);
+    // Instantiate Page object
+    $page = new \tna\Page(['page_title' => 'Upload transfer']);
 
-	return $this->view->render($response, 'upload.html.twig', $page->getPageInfo());
+    return $this->view->render($response, 'upload.html.twig', $page->getPageInfo());
 
 })->setName('upload');
 
-$app->get('/introduction', function($request, $response, $args){
+$app->get('/introduction', function ($request, $response, $args) {
 
-	$page = new \tna\Page(['page_title' => 'Introduction']);
+    $page = new \tna\Page(['page_title' => 'Introduction']);
     return $this->view->render($response, 'introduction.html.twig', $page->getPageInfo());
 
 })->setName('intro');
+
+$app->get('/diff', function ($request, $response, $args) {
+    $page = new \tna\Page(['page_title' => 'Show differences']);
+    return $this->view->render($response, 'diff.html.twig', $page->getPageInfo());
+})->setName('diff');
